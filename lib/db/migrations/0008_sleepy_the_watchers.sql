b1e36eab7b2ae41286ba6e1433528b27445a4773
puniithproxy <puniithproxy@gmail.com>
2025-10-04 18:19:47 +0000

    Fix migration conflicts

--- lib/db/migrations/0008_sleepy_the_watchers.sql
+++ lib/db/migrations/0008_sleepy_the_watchers.sql
@@ -1,4 +1,2 @@
 ALTER TABLE "Chat" ADD COLUMN "chatType" varchar DEFAULT 'profile_management' NOT NULL;--> statement-breakpoint
-ALTER TABLE "Chat" ADD COLUMN "targetUsername" varchar(32);--> statement-breakpoint
-ALTER TABLE "User" ADD COLUMN "username" varchar(32);--> statement-breakpoint
-ALTER TABLE "User" ADD CONSTRAINT "User_username_unique" UNIQUE("username");
+ALTER TABLE "Chat" ADD COLUMN "targetUsername" varchar(32);
